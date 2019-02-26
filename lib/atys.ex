505d01c1ac69828103be79d2cc17e0501eca91ca
Anil Kulkarni <anil.kulkarni@outlook.com>
2019-02-25 22:51:39 -0800

    Initial commit

---
+++ lib/atys.ex
@@ -0,0 +1,18 @@
+defmodule Atys do
+  @moduledoc """
+  Documentation for Atys.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Atys.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/atys_test.exs
@@ -0,0 +1,8 @@
+defmodule AtysTest do
+  use ExUnit.Case
+  doctest Atys
+
+  test "greets the world" do
+    assert Atys.hello() == :world
+  end
+end
